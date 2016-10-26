d70c30612f4acbf54c1bbf97cf3f46a0e0fd97a4
Diane Diaz <ddiaz@microsoft.com>
2016-10-26 13:22:59 -0700

    send auth url in Location header

--- routes/graph-api.js
+++ routes/graph-api.js
@@ -9,7 +9,7 @@ const express = require('express'),
   router = express.Router(),
   database = require('../utils/database'),
   graph = require('msgraph-sdk-javascript'),
-  qs = require('querystring'),
+  //qs = require('querystring'),
   authHelper = require('../utils/authHelper');
 
 router.use((req, res, next) => {
@@ -29,7 +29,8 @@ router.use((req, res, next) => {
   // If no mapped account, send the login information to the client.
   else
     res.header('U-Token', user.userToken);
-    res.status(302).send('Sign in required at /connect.');
+    res.header('Location', 'http://localhost:3000/connect');
+    res.status(302).send('Sign in required.');
 });
 
 
@@ -39,73 +40,72 @@ router.use((req, res, next) => {
 router.get('/getJournal', (req, res) => {
 
   // Check whether the user is authenticated and has a mapped Microsoft login.  
-  const user = (req.user) ? req.user : database.users.findOne({ 'userToken' : userToken });
-  res.header('U-Token', user.userToken);
-  res.status(200).send('some OneNote json');
-});
-//   if (req.isAuthenticated()) {
-//     let client = graph.init({
-//       defaultVersion: 'beta',
-//       debugLogging: true,
-//       authProvider: function (done) {
-//         done(null, user.accessToken);
-//       }
-//     });
+  const user = (req.user) ? req.user.user : database.users.findOne({ 'userToken' : userToken }); //db user for testing todo: remove
+  const accessToken = authHelper.prototype.ensureAuthenticated(); // need to await this for refresh token
+  if (req.isAuthenticated()) {
+    let client = graph.init({
+      defaultVersion: 'beta',
+      debugLogging: true,
+      authProvider: function (done) {
+        done(null, accessToken);
+      }
+    });
+    
+    // Get the journal section.
+    //https://graph.microsoft.com/beta/me/notes/sections?filter=name%20eq%20'Alex%20Darrow''s%20journal'
+    client.api('/me/notes/sections')
+      .filter('name eq \'' + user.displayName + '\'\'s journal\'')
+      .select('name,id')
+      .get((err, response) => {
+        if (err) {
+          res.status(err.status).send(err.message); //does this also end thread?
+        }
+        let sections = response.value;
 
-//     // Get the journal section.
-//     //https://graph.microsoft.com/beta/me/notes/sections?filter=name%20eq%20'Alex%20Darrow''s%20journal'
-//     client.api('/me/notes/sections')
-//       .filter('name eq \'' + user.displayName + '\'\'s journal\'')
-//       .select('name,id')
-//       .get((err, response) => {
-//         if (err) {
-//           res.status(err.status).send(err.message); //does this also end thread?
-//         }
-//         let sections = response.value;
-
-//         // If the journal section exists, get the last three pages. 
-//         // Expanding on pages is not supported for Office 365 notebooks, so this call is made separately.
-//         if (sections.length > 0) {
-//           const section = sections[0];
-//           const sectionId = section.id;
-//           const pages = [];
-//           //client.api('me/notes/pages?filter=/parentSection/id eq \'' + sectionId + '\'')
-//             //.select('title,createdTime,content')
-//             //.top(3)
-//           client.api('/me/notes/sections/' + sectionId + '/pages')
-//             .select('title,createdTime,pageContentUrl')
-//             .orderby('lastModifiedTime desc')
-//             .top(3)
-//             .get((err, response) => {
-//               if (err) {
-//                 res.status(err.status).send(err.message);
-//                 // res.render('error', { message: err.message, error: err });
-//                 // return;
-//               }
-//               //loop pages = response.value;
-//               const html = '<div><p><b>hello </b></p></div><div><p><i>test page html</i></p></div>';
-//               // get and prep page content
-//               res.render('journal', { user: user, section: section.name, pages: pages, test: html });
-//           });
-//       }  
+        // If the journal section exists, get the last three pages. 
+        // Expanding on pages is not supported for Office 365 notebooks, so this call is made separately.
+        if (sections.length > 0) {
+          const section = sections[0];
+          const sectionId = section.id;
+          const pages = [];
+          //client.api('me/notes/pages?filter=/parentSection/id eq \'' + sectionId + '\'')
+            //.select('title,createdTime,content')
+            //.top(3)
+          client.api('/me/notes/sections/' + sectionId + '/pages')
+            .select('title,createdTime,pageContentUrl')
+            .orderby('lastModifiedTime desc')
+            .top(3)
+            .get((err, response) => {
+              if (err) {
+                res.status(err.status).send(err.message);
+                // res.render('error', { message: err.message, error: err });
+                // return;
+              }
+              //loop pages = response.value;
+              // const html = '<div><p><b>hello </b></p></div><div><p><i>test page html</i></p></div>';
+              // // get and prep page content
+              // res.render('journal', { user: user, section: section.name, pages: pages, test: html });
+                res.status(200).send({ection: section.name, pages: pages});
+          });
+      }  
       
-//       // Otherwise create the journal section and the initial page.
-//       // Then redirect back to this route.
-//       // This should only be required only on first visit to this page.
-//       else
-//         //let sectionName = qs.escape(user.displayName + ' \'s journal');
-//         client.api('/me/notes/pages?sectionName=' + user.displayName + '\' journal').post((err, response) => { //errs here
-//           if (err) {
-//             res.render('error', { message: err.message, error: err });
-//             return;
-//           }
-//           // res.header('U-Token', user.userToken);
-//           else res.redirect('/graph');
-//         });
-//     });
-//   }
-//   else
-//     res.redirect('/connect');
-// });
+      // Otherwise create the journal section and the initial page.
+      // Then redirect back to this route.
+      // This should only be required only on first visit to this page.
+      else
+        //let sectionName = qs.escape(user.displayName + ' \'s journal');
+        client.api('/me/notes/pages?sectionName=' + user.displayName + '\' journal').post((err, response) => { //errs here
+          if (err) {
+            res.render('error', { message: err.message, error: err });
+            return;
+          }
+          // res.header('U-Token', user.userToken);
+          else res.redirect('/graph');
+        });
+    });
+  }
+  else
+    res.redirect('/connect');
+});
 
 module.exports = router;
